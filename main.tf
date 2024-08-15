efafea168042f6950144a0befecbf1bf84c1f8cb
alex <lykhogodalex@gmail.com>
2024-08-15 11:05:34 +0300

    test buid 4

--- main.tf
+++ main.tf
@@ -20,36 +20,36 @@ provider "aws" {
   # access_key = data.vault_generic_secret.aws_key.data["accesskey"]
   # secret_key = data.vault_generic_secret.aws_key.data["secretkey"]
 }
-resource "aws_instance" "example2_server" {
-  ami           = "ami-04e914639d0cca79a"
+resource "aws_instance" "ubuntu_server" {
+  ami           = "ami-04a81a99f5ec58529"
   instance_type = "t2.micro"
 
   tags = {
     Name = "Example"
   }
 }
-resource "aws_security_group" "instance_sg" {
-  name        = "allow_ssh_http"
-  description = "Allow SSH and HTTP"
+# resource "aws_security_group" "instance_sg" {
+#   name        = "allow_ssh_http"
+#   description = "Allow SSH and HTTP"
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
